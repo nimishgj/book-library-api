250be20cb7defd62fe0e9f8951ce37993470d498
Nimisha G J <nimisha.int24@infraspec.dev>
2024-01-22 23:45:44 +0530

    test: Add tests for printing the list of books

--- src/test/java/dev/infraspec/BookLibraryTest.java
+++ src/test/java/dev/infraspec/BookLibraryTest.java
@@ -1,16 +1,29 @@
 package dev.infraspec;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
 public class BookLibraryTest {
+    private static BookRepository bookRepository;
+
+    @BeforeAll
+    static void setup() {
+        List<Book> books = Arrays.asList(new Book("someTitle"), new Book("anotherTitle"));
+        bookRepository = new BookRepository(books);
+    }
+
     @Test
     @DisplayName("Print Welcome Message")
     public void printValidWelcomeMessage() {
         DisplayManager displaySpy = spy(new DisplayManager());
         BookLibrary library = new BookLibrary(displaySpy);
+
         library.startApplication();
 
         verify(displaySpy).printWelcomeMessage();
@@ -21,8 +34,20 @@ public class BookLibraryTest {
     public void printListOfBooks() {
         DisplayManager display = spy(new DisplayManager());
         BookLibrary library = new BookLibrary(display);
+
         library.startApplication();
 
-        verify(display).print("List Of Books\n-aBook\n-anotherBook");
+        verify(display).printBookList(anyList());
+    }
+
+    @Test
+    @DisplayName("Print List of Books from given Book Repository")
+    public void printListOfBooksByGivenBookRepository() {
+        DisplayManager display = spy(new DisplayManager());
+        BookLibrary library = new BookLibrary(display, bookRepository);
+
+        library.startApplication();
+
+        verify(display).printBookList(bookRepository.getAllBooks());
     }
 }
--- src/test/java/dev/infraspec/ConsoleDisplayTest.java
+++ src/test/java/dev/infraspec/ConsoleDisplayTest.java
@@ -1,10 +1,13 @@
 package dev.infraspec;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,15 +15,19 @@ class ConsoleDisplayTest {
     PrintStream originalSystemOut;
     ByteArrayOutputStream outputStream;
 
-    @Test
-    @DisplayName("Print valid Welcome Message")
-    void printWelcomeMessage() {
+    @BeforeEach
+    public void setup() {
         originalSystemOut = System.out;
         outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
+    }
 
-        DisplayManager consoleDisplay = new DisplayManager();
-        consoleDisplay.printWelcomeMessage();
+    @Test
+    @DisplayName("Print valid Welcome Message")
+    void printWelcomeMessage() {
+        DisplayManager displayManager = new DisplayManager();
+
+        displayManager.printWelcomeMessage();
 
         assertEquals("Welcome to the Library\n", outputStream.toString());
     }
@@ -28,14 +35,26 @@ class ConsoleDisplayTest {
     @Test
     @DisplayName("Print valid message")
     void printMessage() {
-        originalSystemOut = System.out;
-        outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-
-        DisplayManager consoleDisplay = new DisplayManager();
+        DisplayManager displayManager = new DisplayManager();
         String message = "Random Message";
-        consoleDisplay.print(message);
+
+        displayManager.print(message);
 
         assertEquals(message + "\n", outputStream.toString());
+    }
+
+    @Test
+    @DisplayName("Print List Of Books")
+    void printListOfBooks() {
+        DisplayManager displayManager = new DisplayManager();
+        List<Book> bookList = Arrays.asList(
+                new Book("oneTitle"),
+                new Book("anotherTitle")
+        );
+
+        displayManager.printBookList(bookList);
+        String expectedString = "List Of Books\n- oneTitle\n- anotherTitle\n";
+
+        assertEquals(expectedString, outputStream.toString());
     }
 }
