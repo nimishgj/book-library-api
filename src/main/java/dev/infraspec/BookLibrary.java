4dff70e1950785f0f821e12d4382e53b50e25f2c
Nimisha G J <nimisha.int24@infraspec.dev>
2024-01-25 02:14:51 +0530

    refactor: Remove the extra constructor from BookLibrary

--- src/main/java/dev/infraspec/BookLibrary.java
+++ src/main/java/dev/infraspec/BookLibrary.java
@@ -1,24 +1,11 @@
 package dev.infraspec;
 
-import dev.infraspec.options.CheckoutOption;
-import dev.infraspec.options.ExitOption;
-import dev.infraspec.options.ListBooksOption;
-
-import java.util.Arrays;
-
 public class BookLibrary {
     private final ConsoleManager consoleManager;
     private final BookRepository bookRepository;
     private final Menu menu;
-
-    public BookLibrary() {
-        this.consoleManager = new ConsoleManager();
-        this.bookRepository = BookRepository.defaultBookRepository();
-        this.menu = new Menu(Arrays.asList(new ListBooksOption(), new ExitOption(), new CheckoutOption()), this.bookRepository, this.consoleManager);
-    }
-
-    public BookLibrary(ConsoleManager display, BookRepository bookRepository, Menu menu) {
-        this.consoleManager = display;
+    public BookLibrary(ConsoleManager consoleManager, BookRepository bookRepository, Menu menu) {
+        this.consoleManager = consoleManager;
         this.bookRepository = bookRepository;
         this.menu = menu;
     }
