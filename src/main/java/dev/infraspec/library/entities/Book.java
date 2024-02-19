e4e2da5380e5970ef63879043e61f404801b319b
Nimisha G J <nimisha.int24@infraspec.dev>
2024-02-19 06:18:20 +0530

    [ Progress ] Adds Book entity
    - Define Book class with attributes and constructors
    - Add basic unit tests for Book class
    - Override toString method for Book class

---
+++ src/main/java/dev/infraspec/library/entities/Book.java
@@ -0,0 +1,30 @@
+package dev.infraspec.library.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(int id, String title, String author, int year) {
+        this.author = author;
+        this.id = id;
+        this.title = title;
+        this.year = year;
+    }
+
+    public Book() {
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-5d %-30s %-30s %-10d", id, title, author, year);
+    }
+}
