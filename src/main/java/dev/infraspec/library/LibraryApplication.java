43cb5659f42e7af5d5015e2de7edeb53051bf7cc
Nimisha G J <nimisha.int24@infraspec.dev>
2024-02-19 17:34:05 +0530

    [ Bug Fix ] Fixes Database not found in Book Repository

--- src/main/java/dev/infraspec/library/LibraryApplication.java
+++ src/main/java/dev/infraspec/library/LibraryApplication.java
@@ -2,13 +2,9 @@ package dev.infraspec.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("dev.infraspec.library.repositories")
-@EntityScan("dev.infraspec.library.entities")
 @ComponentScan({"dev.infraspec.library.services", "dev.infraspec.library.repositories","dev.infraspec.library.controllers"})
 public class LibraryApplication {
 
--- src/main/java/dev/infraspec/library/repositories/BookRepository.java
+++ src/main/java/dev/infraspec/library/repositories/BookRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    @Query(nativeQuery = true, value = "SELECT * FROM books")
+    @Query(nativeQuery = true, value = "SELECT * FROM library.books")
     List<Book> getAllBooks();
 }
