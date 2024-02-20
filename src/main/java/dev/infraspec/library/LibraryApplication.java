cdda01d9680e0f6249443d28a8b18466664e0694
nimisha-gj <156162253+nimisha-gj@users.noreply.github.com>
2024-02-20 10:51:45 +0530

    Revert "Implement Database Integration for Getting Books"

--- src/main/java/dev/infraspec/library/LibraryApplication.java
+++ src/main/java/dev/infraspec/library/LibraryApplication.java
@@ -2,10 +2,8 @@ package dev.infraspec.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"dev.infraspec.library.services", "dev.infraspec.library.repositories","dev.infraspec.library.controllers"})
 public class LibraryApplication {
 
 	public static void main(String[] args) {
@@ -13,4 +11,3 @@ public class LibraryApplication {
 	}
 
 }
-
--- src/test/java/dev/infraspec/library/constants/BookTestConstants.java
+++
@@ -1,9 +0,0 @@
-package dev.infraspec.library.constants;
-
-public class BookTestConstants {
-    public static final String SOME_TITLE = "some title";
-    public static final String SOME_AUTHOR = "some author";
-    public static final int SOME_YEAR = 1923;
-    public static final int SOME_ID = 1;
-    public static final String BOOK_TOSTRING_FORMAT = "%-5d %-30s %-30s %-10d";
-}
